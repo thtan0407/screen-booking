ab6728bd5bc10fbf187f263b63f4280e267655a8
Jay B <thtan0407@gmail.com>
2020-09-27 10:43:00 +0700

    Update theme-script.js

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
-    $('.slideBooking').slick({
-        centerMode: true,
-        centerPadding: '15px',
-        slidesToShow: 1, arrows: false,
-    });
+    if ($('.slideBooking').length) {
+        $('.slideBooking').slick({
+            centerMode: true,
+            centerPadding: '15px',
+            slidesToShow: 1, arrows: false,
+        });
+    }
+
+    // $('.profile-form input').focus(function () {
+    //     if($(this))
+    //     $(this).css('font-size', '16px');
+    // })
+
+    if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+        $('.profile-form input').css('font-size', '16px');
+    }
 });
